1e2ede2b6f2dd7275e9a45662f30f2218acaf3ee
Joseph Kessler <kesslerjx@icloud.com>
2022-03-21 08:50:43 -0400

    Create computerPlay function
    Has an array of choices (rock, paper, scissors), gets a random number between 0 and the length of the array, and then uses the number to retreive a choice from the array.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("Hi there!")
+
+function computerPlay() {
+    const choices = ["Rock", "Paper", "Scissors"];
+
+    let random = Math.floor(Math.random() * choices.length);
+    console.log("Random number = " + random);
+    console.log("Random choice = " +choices[random]);
+
+    return choices[random]
+}
