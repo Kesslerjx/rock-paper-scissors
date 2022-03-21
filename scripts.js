1320a63ada83748fcd07e90ab59e7039b6cfb57f
Joseph Kessler <kesslerjx@icloud.com>
2022-03-21 09:11:12 -0400

    Finish round playing function that determines a winner

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
 function computerPlay() {
     const choices = ["Rock", "Paper", "Scissors"];
 
@@ -6,5 +11,27 @@ function computerPlay() {
     console.log("Random number = " + random);
     console.log("Random choice = " +choices[random]);
 
-    return choices[random]
+    return choices[random];
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return "Tie! Try again."
+    }else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        return "You lose! Paper beats rocks."
+    }else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        return "You win! Rock beats scissors."
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You win! Paper beats rock."
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        return "You lose! Scissors beats paper."
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        return "You lose! Rock beats scissors."
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return "You win! Scissors beats paper."
+    } else {
+        return "That's not an option."
+    }
+
 }
